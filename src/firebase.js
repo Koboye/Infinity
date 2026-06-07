26dcf4be18e31013799b7085eb8746595960dac3
Koboye <getachewshambel11@gmail.com>
2026-06-06 22:55:19 -0700

    fix: add googleProvider and facebookProvider exports

--- src/firebase.js
+++ src/firebase.js
@@ -1,19 +1,22 @@
 // firebase.js
 import { initializeApp } from 'firebase/app';
-import { getAuth } from 'firebase/auth';
+import { getAuth, GoogleAuthProvider, FacebookAuthProvider } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_AUTH_DOMAIN",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_STORAGE_BUCKET",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyD9jDk8gijMVAYrsFe4vpojI7GyZnkzGL8",
+  authDomain: "dagu-8348c.firebaseapp.com",
+  projectId: "dagu-8348c",
+  storageBucket: "dagu-8348c.firebasestorage.app",
+  messagingSenderId: "259738670911",
+  appId: "1:259738670911:web:c4d1116e3697a8f67c658a",
+  measurementId: "G-KJW3QQJ26X"
 };
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
 export const storage = getStorage(app);
+export const googleProvider = new GoogleAuthProvider();
+export const facebookProvider = new FacebookAuthProvider();
